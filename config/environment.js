4130786bb1156445e885c9c10ac2fec0ed09cd96
Danny Hadley <dadleyy@gmail.com>
2018-04-05 17:33:24 -0400

    some ci + misc tweaks to blueprint

--- config/environment.js
+++ config/environment.js
@@ -1,8 +1,9 @@
-'use strict';
+/* eslint-env node */
 
 module.exports = function(environment) {
-  let ENV = {
+  const ENV = {
     modulePrefix: 'milky-way',
+    podModulePrefix: 'milky-way/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
@@ -13,14 +14,14 @@ module.exports = function(environment) {
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
   };
 
   if (environment === 'development') {
